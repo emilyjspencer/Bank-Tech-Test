a44c32460e09207dbbcf074c72d3c2f2dd7ba0f1
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-24 11:40:52 +0000

    Refactors test to use a before hook

--- spec/bankaccount_spec.rb
+++ spec/bankaccount_spec.rb
@@ -2,6 +2,10 @@ require 'bankaccount'
 
 describe 'BankAccount' do
   let(:bankaccount) { BankAccount.new }
+
+  before(:each) do
+    bankaccount = BankAccount.new
+  end 
   
   describe '#initialize' do
     it 'initializes with a balance of 2000' do
@@ -11,7 +15,6 @@ describe 'BankAccount' do
 
   describe '#withdraw_cash' do
     it 'allows the account holder to withdraw cash from their account' do
-      bankaccount = BankAccount.new
       bankaccount.withdraw_cash(200)
       expect(bankaccount.balance).to eq 1800
     end
@@ -19,7 +22,6 @@ describe 'BankAccount' do
 
   describe '#deposit' do
   it 'allows the account holder to deposit money to their account' do
-    bankaccount = BankAccount.new
     bankaccount.deposit(250)
     expect(bankaccount.balance).to eq 2250
   end
