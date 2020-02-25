7d925b7d0bc419d7668975f25457183aa28b5c39
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-25 19:47:56 +0000

    Reconfigures simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,19 @@
 require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter '/bin/'
-  add_filter '/db/'
-  add_filter '/spec/' # for rspec
-  add_filter '/test/' # for minitest
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  end
 end
 
 
